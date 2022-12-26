e8b09dbb574a8da8d43eeec3e973347125365a11
Prakhar Tamrakar <121457073+daemon-112@users.noreply.github.com>
2022-12-27 01:15:09 +0530

    Update streamlit_app.py
    Added breakfast menu header and 3 iteams

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 
-streamlit.title('Vamos')
-streamlit.header('WC Champian!!')
-streamlit.text('Trophies: 3')
-streamlit.text('Goat Stuff!')
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3& Blueberry Oatmeal')
+streamlit.text('Kale,Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
